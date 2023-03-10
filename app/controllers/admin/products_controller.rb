caeedb2f3aeaa130f0943d319efd5c515db6ebe0
woodenonesie <olga.pi4ugina@yandex.ru>
2023-03-10 00:53:30 -0800

    add admin authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+  # http_basic_authenticate_with name: 'lol', password: 'kek'
 
   def index
     @products = Product.order(id: :desc).all
