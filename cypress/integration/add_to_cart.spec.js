ad245c5a626fa7e162c846b274f937640f775892
woodenonesie <olga.pi4ugina@yandex.ru>
2023-03-16 05:36:35 -0700

    add tests for adding products to the cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,26 @@
+describe('Add product to the cart', () => {
+
+  beforeEach(() => {
+    cy.visit("/");
+   });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("should navigate to product details page", () => {
+    cy.visit("/");
+    cy.contains(".products article", "Scented Blade")
+    .click()
+    .url().should("include", "products")
+    cy.get(".btn")
+    .click()
+    .get(".end-0 > .nav-link")
+    .should("contain", "My Cart (1)")
+  });
+
+})
